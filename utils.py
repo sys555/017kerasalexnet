cf7b45e1d36ea0ed5ce5e35f83b166cb511923bd
banana <sys555@outlook.com>
2021-12-04 20:46:28 +0800

    change utils to his code

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import cv2
 import tensorflow as tf
 from tensorflow.python.ops import array_ops
 
+
 def load_image(path):
     # 读取图片，rgb
     img = mpimg.imread(path)
@@ -14,17 +15,19 @@ def load_image(path):
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
     return crop_img
 
+
 def resize_image(image, size):
-    with tf.name_scope("resize_image"):
+    with tf.name_scope('resize_image'):
         images = []
         for i in image:
             i = cv2.resize(i, size)
-            image.append(i)
-        images = np.array(image)
+            images.append(i)
+        images = np.array(images)
         return images
 
+
 def print_answer(argmax):
-    with open("./data/model/index_word.txt","r",encoding='utf-8') as f:
+    with open("./data/model/index_word.txt", "r", encoding='utf-8') as f:
         synset = [l.split(";")[1][:-1] for l in f.readlines()]
 
     print(synset[argmax])
